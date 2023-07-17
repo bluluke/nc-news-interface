d8e842745556bf7eedc3f8bd8e923660704a931a
bluluke <bluluke@hotmail.com>
2023-07-17 19:12:14 +0100

    Delete: Show more button because pagination in later ticket.

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,14 +1,26 @@
 import { ArticleCard } from "./ArticleCard"
+import { useEffect, useState } from 'react';
+import { getArticles } from "../utils/api";
+
 export const ArticleList = () => {
 
+const [articleCards, setArticleCards] = useState();
+const [isLoading, setIsloading] = useState(true); 
+useEffect(() => {
+    getArticles().then((articleData) => {
+        setArticleCards(articleData);
+        setIsloading(false);
+    })
+}, []);
+
+if(isLoading) return <p>Loading...</p>;
     return (
-        <div id="articleListContainer"> 
+        <section id="articleListContainer"> 
             <ul id="articleList">
-                <li>
-                    <ArticleCard />
-                </li>
+                {articleCards.map(({ article_id, title, author, topic, created_at, votes, article_img_url, comment_count }) => {
+                        return <ArticleCard key={article_id} title={title} author={author} topic={topic} created_at={created_at} votes={votes} article_img_url={article_img_url} comment_count={comment_count}/>
+                })}
             </ul>
-            <button>Show more</button>
-        </div>
+        </section>
     )
 }
