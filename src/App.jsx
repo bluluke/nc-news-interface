c92da59498aa3fb072af763f347a783cfb0808d7
bluluke <bluluke@hotmail.com>
2023-07-17 12:29:03 +0100

    Change: import statement for Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {Route, Routes} from 'react-router-dom';
 import './App.css'
-import { Nav } from './pages/Nav'
+import { Nav } from './components/Nav'
 import { Home } from './pages/Home'
 function App() {
  
