f3b414519333cdc4a39317781be41202118ec216
bluluke <bluluke@hotmail.com>
2023-07-20 15:24:48 +0100

    Add: sortBy & order user input

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export const Home = () => {
     const [topics, setTopics] = useState();
     const [currentTopic, setCurrentTopic] = useState('all')
     const [isLoading, setIsLoading] = useState(true);
-    const [sortByOrder, setSortByOrder ] = useState('')
+    const [sortByOrder, setSortByOrder ] = useState(null)
 ;    const navigate = useNavigate();
     const currentLocation = useLocation();
   
@@ -25,26 +25,98 @@ export const Home = () => {
     
 const handleSubmit = (e) => {
     e.preventDefault();
-    navigate(`/?topic=${currentTopic}`);
+    navigate(`/?topic=${currentTopic}&&${sortByOrder}`);
 }
 
 const handleTopicChange = (e) => {
     setCurrentTopic(e.target.value);
 };
+const handleSortOrderChange = (e) => {
+    setSortByOrder(e.target.value)
+}
+
+
 
 if(isLoading) return <p>Loading...</p>;
     return (
         <div id="home">  
             <Header />
             <form id="topicInput" onSubmit={handleSubmit}>
-                <input
-                    type='radio'
-                    id='all'
-                    name="topic"
-                    value='all'
-                    onChange={handleTopicChange}
-                />
-                <label htmlFor='all'>all</label>
+                <fieldset>
+                    <legend>sort articles:</legend>
+                    <div>
+                        <input
+                        type="radio"
+                        id="votes_desc"
+                        name="sort_by_order"
+                        value="sort_by=votes&&order=desc"
+                        onChange={handleSortOrderChange}
+                        />   
+                        <label htmlFor="votes_desc">most votes to fewest votes</label>
+                    </div>
+                    <div>
+                        <input
+                        type="radio"
+                        id="votes_asc"
+                        name="sort_by_order"
+                        value="sort_by=votes&&order=asc"
+                        onChange={handleSortOrderChange}
+                        />
+                        <label htmlFor="votes_asc">fewest votes to most votes</label>
+                    </div>
+                    <div>
+                        <input
+                        type="radio"
+                        id="comments_desc"
+                        name="sort_by_order"
+                        value="sort_by=comment_count&&order=desc"
+                        onChange={handleSortOrderChange}
+                        />   
+                        <label htmlFor="comments_desc">most comments to fewest comments</label>
+                    </div>
+                    <div>
+                        <input
+                        type="radio"
+                        id="comments_asc"
+                        name="sort_by_order"
+                        value="sort_by=comment_count&&order=asc"
+                        onChange={handleSortOrderChange}
+                        />   
+                        <label htmlFor="comments_asc">fewest comments to most comments</label>
+                    </div>
+                    <div>
+                        <input
+                        type="radio"
+                        id="created_at_desc"
+                        name="sort_by_order"
+                        value="sort_by=created_at&&order=desc"
+                        onChange={handleSortOrderChange}
+                        />   
+                        <label htmlFor="created_at_desc">most recent to least recent</label>
+                    </div>
+                    <div>
+                        <input
+                        type="radio"
+                        id="created_at_asc"
+                        name="sort_by_order"
+                        value="sort_by=created_at&&order=asc"
+                        onChange={handleSortOrderChange}
+                        />   
+                        <label htmlFor="created_at_asc">least recent to most recent</label>
+                    </div>
+                </fieldset>
+                <fieldset>
+                <legend>article topics:</legend>    
+                <div>
+                    <input
+                        type='radio'
+                        id='all'
+                        name="topic"
+                        value='all'
+                        onChange={handleTopicChange}
+                    />
+                    <label htmlFor='all'>all</label>
+                </div>
                 {topics.map((topic) => {
                     return (
                     <div key={topic}>
@@ -55,13 +127,16 @@ if(isLoading) return <p>Loading...</p>;
                         onChange={handleTopicChange}
                         />
                         <label htmlFor={topic}>{topic}</label>
+                                
                     </div>
                     )
-            })}
-            <button>Search</button>
+
+                })}
+                </fieldset>
+            <button type="submit">Search</button>
             </form>
-            <ArticleList currentTopic={currentTopic} currentLocation={currentLocation} sortByOrder={sortByOrder}/>
             <ArticleListHeading currentTopic={currentTopic} currentLocation={currentLocation}/>
+            <ArticleList currentTopic={currentTopic} currentLocation={currentLocation} sortByOrder={sortByOrder} setSortByOrder={setSortByOrder}/>
         </div>
     )
 }
