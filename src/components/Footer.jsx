44a774b83b6f70545c9cc85b943b2d33d3ad4915
bluluke <bluluke@hotmail.com>
2023-08-19 15:29:04 +0100

    Update: capitalise footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
     return  (
         <footer id='footer'>
-            <a href="#navbar">back to the top</a> 
+            <a href="#navbar">Back to the top</a> 
         </footer>
     )
 }
